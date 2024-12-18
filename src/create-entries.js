79975d98219bdce12683fcef59db0de7746bffac
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-18 23:52:12 +0300

    Included Created Todo In Default Project

--- src/create-entries.js
+++ src/create-entries.js
@@ -3,22 +3,19 @@ import { formHandling } from './dom-manipulation.js';
 import { projectManager } from './project-manager.js';
 import todoManager from './todoManager.js';
 import { getUserInput } from './user-input.js';
-import { displayProjects } from './display-controller.js';
-
-export let createdProject = null;
+import { defaultProject } from './index.js';
 
 export const createUserProject = function(event){
     formHandling.closeForm(event);
     let input = getUserInput().projectName;
     const newProject = projectManager.createNewProject(input);
-    displayProjects(input);
 };
 
 export const createUserTodo = function(event){
     formHandling.closeForm(event);
     let input = getUserInput();
-    const newTodo = todoManager.createNewTodo(input);
-    console.log(newTodo);
+    let createdTodo = todoManager.createNewTodo(input);
+    projectManager.addTodo(defaultProject, createdTodo)
 }
 
 projectFormControls.projectConfirmBtn.addEventListener('click', createUserProject);
