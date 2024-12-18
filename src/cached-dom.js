113deb8bb0369edd6ff5140f7cdaddb063a3e815
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-18 11:05:35 +0300

    Select Form Input Nodes

--- src/cached-dom.js
+++ src/cached-dom.js
@@ -3,17 +3,39 @@ const newTodoBtn = document.querySelector('#newTodo');
 const todoCancelBtn = document.querySelector('#todoCancel');
 const todoConfirmBtn = document.querySelector('#todoConfirm');
 const projectCancelBtn = document.querySelector('#projectCancel');
-const projectCofirmBtn = document.querySelector('#projectConfirm');
+const projectConfirmBtn = document.querySelector('#projectConfirm');
 const todoDialog = document.querySelector('#todoDialog')
 const projectDialog = document.querySelector('#projectDialog')
+
+const projectNameInput = document.querySelector('#projectName')
+const titleInput = document.querySelector('#title');
+const desciptionInput = document.querySelector('#description');
+const dueDateInput = document.querySelector('#dueDate');
+const priorityInput = document.querySelector('#priority');
+const notesInput = document.querySelector('#notes');
+const statusInput = document.querySelector('#status');
+
 
 export const domNodes = {
     newProjectBtn,
     newTodoBtn,
-    todoCancelBtn,
-    todoConfirmBtn,
-    projectCancelBtn,
-    projectCofirmBtn,
     todoDialog,
     projectDialog,
+}
+
+export const todoFormControls = {
+    titleInput,
+    desciptionInput,
+    dueDateInput,
+    priorityInput,
+    notesInput,
+    statusInput,
+    todoCancelBtn,
+    todoConfirmBtn,
+}
+
+export const projectFormControls = {
+    projectNameInput,
+    projectCancelBtn,
+    projectConfirmBtn,
 }
--- src/user-input.js
+++ src/user-input.js
@@ -1,33 +1,22 @@
-export const getUserInput = function(){
-    let projectConfirmation = confirm('Do you want to create a new project');
-    if (projectConfirmation){
-        let projectName = prompt('What is the name of your project?');
-        let todoConfirmation = confirm('Do you want to create a new todo?')
-        if (todoConfirmation){
-            let title = prompt('What is the title of your todo?')
-            let description = prompt('Can you provide a desciption of your todo?')
-            let dueDate = prompt('When is your todo due?')
-            let priority = prompt('Can you provide the priority level of this todo?')
-            let notes = prompt('Are there additional notes on this todo to provide?')
-            let status = prompt('What is the status of this todo?')
+import { domNodes } from './cached-dom.js';
 
-            return {
-                projectName,
-                title,
-                description,
-                dueDate,
-                priority,
-                notes,
-                status
-            }
-        }
-        else {
-            return {
-                projectName,
-            }
-        }
-    }
-    else {
-        return null;
+domNodes.projectConfirmBtn.addEventListener('click', getProjectName);
+domNodes.todoConfirmBtn.addEventListener('click', getUserInput)
+export const getUserInput = function(){
+    let title = prompt('What is the title of your todo?')
+    let description = prompt('Can you provide a desciption of your todo?')
+    let dueDate = prompt('When is your todo due?')
+    let priority = prompt('Can you provide the priority level of this todo?')
+    let notes = prompt('Are there additional notes on this todo to provide?')
+    let status = prompt('What is the status of this todo?')
+
+    return {
+        projectName,
+        title,
+        description,
+        dueDate,
+        priority,
+        notes,
+        status
     }
 }
