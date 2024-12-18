429615ff7c1dd6255afb96bbc73a18677cd94e2e
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-18 10:28:03 +0300

    Cache The Dom Elements
    Caching them separately makes it easier to manipulate
    them

---
+++ src/cached-dom.js
@@ -0,0 +1,16 @@
+const newProjectBtn = document.querySelector('#newProject');
+const newTodoBtn = document.querySelector('#newTodo');
+const todoCancelBtn = document.querySelector('#todoCancel');
+const todoConfirmBtn = document.querySelector('#todoConfirm');
+const projectCancelBtn = document.querySelector('#projectCancel');
+const projectCofirmBtn = document.querySelector('#projectConfirm');
+
+
+export const domNodes = {
+    newProjectBtn,
+    newTodoBtn,
+    todoCancelBtn,
+    todoConfirmBtn,
+    projectCancelBtn,
+    projectCofirmBtn,
+}
