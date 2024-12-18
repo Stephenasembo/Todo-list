3fb1d044fae073418c2dbff511a62bc1d982e1ab
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-18 10:01:14 +0300

    Remove Console Tests

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 import todoManager from './todoManager.js';
 import { projectManager } from './project-manager.js';
 import { getUserInput } from './user-input.js';
-
-let obj = {
-    projectName: 'Introduction',
-    title: 'hello world',
-    description: 'goodbye world',
-    dueDate: 2024,
-    priority: 'urgent',
-    notes: 'none',
-    status: 'done',
-}
-let newTodo = todoManager.createNewTodo(obj);
-console.log(newTodo);
-
-let project1 = projectManager.createNewProject(obj.projectName);
-projectManager.addTodo(project1, newTodo);
-projectManager.addTodo(project1, newTodo);
-console.log(project1);
