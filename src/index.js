68764ac09818b2445998a75dfd670c3480473dc5
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-18 21:25:43 +0300

    Create A Default Project
    This is what will be displayed on initial loading of
    the page

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import { getUserInput } from './user-input.js';
 import { formHandling } from './dom-manipulation.js';
 import { createUserProject, createUserTodo } from './create-entries.js';
 import './styles.css';
+import { displayProjects } from './display-controller.js';
+
+let defaultValues = {
+    title: 'Learn Programming',
+    description: 'I want to be a web developer',
+    dueDate: '2025',
+    priority: 'Important',
+    notes: 'I am learning using The Odin Project curriculum',
+    status: 'In porgress',
+}
+
+let defaultProject = projectManager.createNewProject('Default Project');
+let defaultProjectName = defaultProject.name
+displayProjects(defaultProjectName);
