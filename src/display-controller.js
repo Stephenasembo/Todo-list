695289226c63db8dc050b23ca4b7665d7c838697
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-19 00:22:21 +0300

    Project And It's Todos Will Be Displayed Together

--- src/display-controller.js
+++ src/display-controller.js
@@ -1,7 +1,32 @@
 import { displayNodes } from './cached-dom';
 
-export const displayProjects = function(text){
-    const para = document.createElement('p');
-    para.textContent = text;
-    displayNodes.projectsDiv.appendChild(para);
+export const updateDisplay = function (){
+    const projectDiv = document.createElement('div');
+    function newProject(text){
+        const heading = document.createElement('h2');
+        heading.textContent = text;
+        projectDiv.appendChild(heading);    
+    }
+    function newTodo(object){
+        const todoDiv = document.createElement('div');
+        const titlePara = document.createElement('p');
+        const dueDatepara = document.createElement('p');
+        const priorityPara = document.createElement('p');
+    
+        titlePara.textContent = object.title;
+        dueDatepara.textContent = object.dueDate;
+        priorityPara.textContent = object.priority;
+    
+        todoDiv.appendChild(titlePara);
+        todoDiv.appendChild(dueDatepara);
+        todoDiv.appendChild(priorityPara);
+    
+        projectDiv.appendChild(todoDiv);
+    }
+
+    displayNodes.projectsDiv.appendChild(projectDiv);
+    return {
+        newProject,
+        newTodo,
+    }
 }
