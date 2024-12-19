438f7f0ad26a30b30aa5f8c04e2d7e29de760ec4
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2024-12-19 11:48:03 +0300

    Automatically Push Todos To Default Project

--- src/create-entries.js
+++ src/create-entries.js
@@ -4,6 +4,7 @@ import { projectManager } from './project-manager.js';
 import todoManager from './todoManager.js';
 import { getUserInput } from './user-input.js';
 import { defaultProject } from './index.js';
+import { userProjects } from './choose-project.js';
 
 export const createUserProject = function(event){
     formHandling.closeForm(event);
@@ -15,7 +16,8 @@ export const createUserTodo = function(event){
     formHandling.closeForm(event);
     let input = getUserInput();
     let createdTodo = todoManager.createNewTodo(input);
-    projectManager.addTodo(defaultProject, createdTodo)
+    projectManager.addTodo(userProjects[0], createdTodo);
+    console.log(userProjects)
 }
 
 projectFormControls.projectConfirmBtn.addEventListener('click', createUserProject);
